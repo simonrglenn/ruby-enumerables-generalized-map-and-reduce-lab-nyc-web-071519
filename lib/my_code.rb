d3078bd23e55bbc44769b72bed12e4bba55939aa
Simon Glenn <simonrglenn@gmail.com>
2019-07-10 19:42:34 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
 # Your Code Here
+
+def map (array)
+  new = []
+  i = 0
+  while i < array.length do
+    new.push(yield(array[i]))
+    i +=1
+  end
+  new
+end
+
+
+
+
+
+
+def reduce (array, value* )
+  i = 0
+  while i < array.length do
+    yield(value)
+    i +=1
+  end
+end
